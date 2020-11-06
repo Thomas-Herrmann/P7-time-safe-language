bac6ce03150c7ed979de6f93c05ba9b4c2139af9
Spidercoder <Spidercoder@users.noreply.github.com>
2020-11-06 13:22:10 +0100

    Added query support to Uppaal XML

--- translation/app/Main.hs
+++ translation/app/Main.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Ast
 import Partition as P
-
+import Text.XML
+import Uppaal
 
 ex = AppExp 
         (ValExp (MatchVal (MultiMatch (RefPat "y") (AppExp (ValExp (TermVal "Single" [])) (RefExp "y")) (SingleMatch (RefPat "x") (RefExp "x"))))) 
@@ -13,8 +16,22 @@ ex' = AppExp
         (ValExp (MatchVal (SingleMatch (RefPat "x") (RefExp "x"))))
         (ValExp (TermVal "True" []))
 
+tem = Template { temName = "Test Template"
+               , temLocations = [ Location "id0" [] (Just "hehe")
+                                , Location "id1" [] (Just "xd")]
+               , temTransitions = [Transition "id0" "id1" []]
+               , temDecls = ["These are template declarations"]
+               , temInit = "id0"}
+
+sys = System { sysDecls = [ "chan pSen, pSen2, rSen, pLig, wgSen, wgRec, rRec;"
+                          , "bool pSenval = false, pSenval2 = false;"
+                          , "bool ow = false, ow2 = false;"]
+             , sysTemplates = [tem]
+             , sysSystemDecls = ["This is a system declaration"]
+             , sysQueries = [Query "This is a query" "This is a comment"] }
 
 main :: IO ()
 main = do 
     print $ show ex
     print $ show (partition ex 0)
+--main = Text.XML.writeFile def "test.xml" $ systemToXML sys
