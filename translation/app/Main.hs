33d10abc54d63af33f26185251f84e72ffce95a0
Thomas Herrmann <48517973+Thomas-Herrmann@users.noreply.github.com>
2020-11-09 11:08:18 +0100

    Implemented translation of let expressions.

--- translation/app/Main.hs
+++ translation/app/Main.hs
@@ -31,8 +31,12 @@ sys = System { sysDecls = [ "chan pSen, pSen2, rSen, pLig, wgSen, wgRec, rRec;"
              , sysSystemDecls = ["This is a system declaration"]
              , sysQueries = [Query "This is a query" "This is a comment"] }
 
+
 main :: IO ()
-main = do 
-    print $ show ex
-    print $ show (partition ex 0)
+--main = do 
+--    print $ show ex
+--    print $ show (partition ex 0)
 --main = Text.XML.writeFile def "test.xml" $ systemToXML sys
+main = case translate (LetExp "x" (ValExp (ConVal OpenCon)) (RefExp "x")) [] [] [] "test" of
+        Nothing  -> print "failure"
+        Just sys -> Text.XML.writeFile def "test.xml" $ systemToXML sys
--- translation/src/Translate.hs
+++ translation/src/Translate.hs
@@ -38,18 +38,76 @@ translate e clockNames inPinNames outPinNames worldName =
         e'           = substitute e $ clockSubst `Map.union` inPinsSubst `Map.union` outPinsSubst `Map.union` worldSubst
 
 
-translateExp :: [Text] -> Exp -> TransT (Template, System)
-translateExp inVars (ValExp _) = do 
+joinSys :: System -> System -> System
+sys1 `joinSys` sys2 = sys1{ 
+    sysDecls       = sysDecls sys1 ++ sysDecls sys2,
+    sysTemplates   = sysTemplates sys1 ++ sysTemplates sys2,
+    sysSystemDecls = sysSystemDecls sys1 ++ sysSystemDecls sys2,
+    sysQueries     = sysQueries sys1 ++ sysQueries sys2
+ }
+
+
+joinTemp :: Template -> Template -> Template
+temp1 `joinTemp` temp2 = temp1{
+    temLocations   = temLocations temp1 ++ temLocations temp2,
+    temTransitions = temTransitions temp1 ++ temTransitions temp2,
+    temDecls       = temDecls temp1 ++ temDecls temp2
+}
+
+
+nilSystem :: TransT (Template, System)
+nilSystem = do
     name  <- nextTempName
     locID <- nextLocID
-    let loc = Location locID [] $ Just locID
+    let loc = Location locID [] $ Just (Text.cons 'L' locID)
     return (Template name [loc] [] [] locID locID, System [] [] [] [])
+
+
+translateExp :: [Text] -> Exp -> TransT (Template, System)
+translateExp _ (ValExp _) = nilSystem
+translateExp _ (FixExp (ValExp (MatchVal (SingleMatch (RefPat _) _)))) = nilSystem
+
+--translateExp inVars (AppExp e1 e2) = do
+--    (temp1, sys1)     <- translateExp inVars e1
+--    (temp2, sys2)     <- translateExp inVars e2
+--    id1               <- nextUniqueID
+--    let (vals1, id1') = Partition.partition e1 id1
+--    setUniqueID id1'
+--    id2               <- nextUniqueID
+--    let (vals2, id2') = Partition.partition e2 id2
+--    setUniqueID id2'
+--    -- TODO: apply!
+
+--translateExp inVars (InvarExp g xs _ e1 e2) = do
+--  -- TODO!!
+
+translateExp inVars (LetExp x e1 e2) = do
+    (temp1, sys1)     <- translateExp inVars e1
+    id1               <- nextUniqueID
+    case Partition.partition e1 id1 of
+        Nothing            -> mzero
+        Just (vals1, id1') -> do
+            setUniqueID id1'
+            systems           <- Prelude.sequence [translateExp inVars (substitute e2 (Map.singleton x v)) | v <- Set.toList vals1]   
+            finalLocID        <- nextLocID
+            let finalLoc      = Location finalLocID [] $ Just (Text.cons 'L' finalLocID)
+            return $ Prelude.foldr (mergeSystems (temFinal temp1)) (temp1{ temFinal = finalLocID, temLocations = finalLoc:(temLocations temp1) }, sys1) systems
+    where
+        mergeSystems from (currTemp, currSys) (exisTemp, exisSys) = 
+            let newTemp        = exisTemp `joinTemp` currTemp
+                newTransitions = [Transition from (temInit currTemp) [], Transition (temFinal currTemp) (temFinal exisTemp) []]
+            in (newTemp{ temTransitions = temTransitions newTemp ++ newTransitions }, exisSys `joinSys` currSys)
 
 
 nextUniqueID :: TransT Integer
 nextUniqueID = State.get >>= 
-    (\state -> State.put  state{ uniqueID = ((uniqueID state) + 1) } >> 
+    (\state -> State.put state{ uniqueID = ((uniqueID state) + 1) } >> 
     (return (uniqueID state)))
+
+
+setUniqueID :: Integer -> TransT ()
+setUniqueID n = State.get >>=
+    (\state -> State.put state{ uniqueID = n })
 
 
 nextTempName :: TransT Text
